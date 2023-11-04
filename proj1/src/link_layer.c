2986c2c94299c822169a284041d69ed59d9c5192
Tonevanda <jmguellourenco@gmail.com>
2023-11-04 19:11:40 +0000

    Add function to get baud rate and refactor code in link_layer.c

--- proj1/src/link_layer.c
+++ proj1/src/link_layer.c
@@ -90,6 +90,61 @@ struct termios newtio;
 int filePort;
 
 
+speed_t getBaudRate(int baud){
+    switch (baud) {
+        case 110:
+            return B110;
+        case 300:
+            return B300;
+        case 600:
+            return B600;
+        case 1200:
+            return B1200;
+        case 2400:
+            return B2400;
+        case 4800:
+            return B4800;
+        case 9600:
+            return B9600;
+        case 19200:
+            return B19200;
+        case 38400:
+            return B38400;
+        case 57600:
+            return B57600;
+        case 115200:
+            return B115200;
+        case 230400:
+            return B230400;
+        case 460800:
+            return B460800;
+        case 500000:
+            return B500000;
+        case 576000:
+            return B576000;
+        case 921600:
+            return B921600;
+        case 1000000:
+            return B1000000;
+        case 1152000:
+            return B1152000;
+        case 1500000:
+            return B1500000;
+        case 2000000:
+            return B2000000;
+        case 2500000:
+            return B2500000;
+        case 3000000:
+            return B3000000;
+        case 3500000:
+            return B3500000;
+        case 4000000:
+            return B4000000;
+        default: 
+            return -1;
+    }
+}
+
 int connect(const char *serialPort){
     // Open serial port device for reading and writing and not as controlling tty
     // because we don't want to get killed if linenoise sends CTRL-C.
@@ -224,61 +279,6 @@ int changeFrameBack(unsigned char* frame, int index, int originalByte){
     frame[index] = originalByte;
     printf("The error was changed back at packet[%d] to %02X\n", index, frame[index]);
     return 0;
-}
-
-speed_t getBaudRate(int baud){
-    switch (baud) {
-        case 110:
-            return B110;
-        case 300:
-            return B300;
-        case 600:
-            return B600;
-        case 1200:
-            return B1200;
-        case 2400:
-            return B2400;
-        case 4800:
-            return B4800;
-        case 9600:
-            return B9600;
-        case 19200:
-            return B19200;
-        case 38400:
-            return B38400;
-        case 57600:
-            return B57600;
-        case 115200:
-            return B115200;
-        case 230400:
-            return B230400;
-        case 460800:
-            return B460800;
-        case 500000:
-            return B500000;
-        case 576000:
-            return B576000;
-        case 921600:
-            return B921600;
-        case 1000000:
-            return B1000000;
-        case 1152000:
-            return B1152000;
-        case 1500000:
-            return B1500000;
-        case 2000000:
-            return B2000000;
-        case 2500000:
-            return B2500000;
-        case 3000000:
-            return B3000000;
-        case 3500000:
-            return B3500000;
-        case 4000000:
-            return B4000000;
-        default: 
-            return -1;
-    }
 }
 
 ////////////////////////////////////////////////
